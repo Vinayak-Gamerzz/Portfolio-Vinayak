189d12b0ecec0153ab6fd3e8e8761ba780087266
Dell <Vinayaktiwari307@gmail.com>
2026-06-25 19:19:42 +0530

    Deleted everything restarting again

--- script.js
+++
@@ -1,8 +0,0 @@
-const themeToggle = document.querySelector(".theme-toggle");
-
-const savedTheme = localStorage.getItem("vinayak-theme");
-if (savedTheme) {
-  root.dataset.theme = savedTheme;
-} else if (window.matchMedia("(prefers-color-scheme: light)").matches) {
-  root.dataset.theme = "light";
-}
